fa7f8169abd762ee3d1ec6116d6540a9c7be39f4
CodeMentorsOfficial <84150944+CodeMentorsOfficial@users.noreply.github.com>
2021-05-15 00:38:34 -0600

    Add files via upload

---
+++ src/util/a.js
@@ -0,0 +1,24 @@
+console.log("Loading... a.js");
+module.exports.a = {
+    "openapps" : ["stapps"],
+    "closeapps" : ["endapps"],
+    "apply" : ["a+"],
+    "play" : ["p", "pl"],
+    "skip" : ["s"],
+    "resume" : ["res", "r"],
+    "pause" : ["pp"],
+    "join" : ["enter", "fuckon", "waxon", "appear"],
+    "leave" : ["fuckoff", "waxoff", "disappear"],
+    "queue" : ["q"],
+    "selopenapps" : ["sopenapps"],
+    "selcloseapps" : ["scloseapps"],
+    "selapply" : ["sapply"],
+    "say" : [],
+    "verify" : ["verifyme"],
+    "cmds" : ["commands"],
+    "alliases" : ["als"],
+    "help" : [],
+    "announcement" : ["announce", "shout", "important"]
+}
+
+console.log("a.js loaded... Success!");
---
+++ src/util/c.js
@@ -0,0 +1,24 @@
+console.log("Loading... c.js");
+module.exports.c = [
+    "openapps",
+    "closeapps",
+    "apply",
+    "play",
+    "skip",
+    "resume",
+    "pause",
+    "join",
+    "leave",
+    "queue",
+    "selopenapps",
+    "selcloseapps",
+    "selapply",
+    "say",
+    "verify",
+    "cmds",
+    "alliases",
+    "help",
+    "announcement"
+]
+
+console.log("c.js loaded... Success!");
